6fdca62259f671d8561d885a804cf2059831d155
Mevan Samaratunga <mevansam@gmail.com>
2015-06-08 14:22:18 -0400

    fixed paths

--- bosh/config.go
+++ bosh/config.go
@@ -2,6 +2,8 @@ package bosh
 
 import (
 	"golang.org/x/net/context"
+	
+	"github.com/mevansam/terraform-provider-bosh/bosh/bosh_client"
 )
 
 type Config struct {
@@ -10,9 +12,9 @@ type Config struct {
 	Password string
 }
 
-func (c *Config) Client() (*BoshClient, error) {
+func (c *Config) Client() (*bosh_client.Director, error) {
 	
-	b, err := NewBoshClient(context.Background(), c.Target, c.User, c.Password)
+	b, err := bosh_client.NewDirector(context.Background(), c.Target, c.User, c.Password)
 	if err != nil && err.Error() != "bosh director not found" {
 		return nil, err
 	}
